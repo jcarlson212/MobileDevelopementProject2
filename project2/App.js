71109f03ca7f413122319e38be36ec7f9e3b8b35
jcarlson212 <44142882+jcarlson212@users.noreply.github.com>
2020-05-28 21:31:17 -0700

    Now there are tabs and a settings screen
    The different screens will eventually be moved into separate files

--- project2/App.js
+++ project2/App.js
@@ -2,6 +2,31 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+
+class HomeScreen extends React.Component {
+
+  render() {
+    return(<View style={styles.container}>
+          <Text>Search for a movie</Text>
+          <TextInput style={styles.searchInput} onChange={text => handleSearchChange(text)} value={() => this.state.searchText}/>
+          <Button title="Submit" onPress={() => this.search()}/>
+        </View>);
+  }
+}
+
+class SettingsScreen extends React.Component {
+
+  render() {
+    return(<View style={styles.container}>
+          <Text>Settings</Text>
+        </View>);
+  }
+}
 
 export default class App extends React.Component {
 
@@ -21,11 +46,35 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <View style={styles.container}>
-          <Text>Search for a movie</Text>
-          <TextInput style={styles.searchInput} onChange={text => handleSearchChange(text)} value={() => this.state.searchText}/>
-          <Button title="Submit" onPress={() => this.search()}/>
-        </View>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+
+
+
+
       </NavigationContainer>
       
     );
