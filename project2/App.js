f32d1fd5520a99326506a7c5fbdbed8064064edc
jcarlson212 <44142882+jcarlson212@users.noreply.github.com>
2020-05-29 19:55:48 -0700

    Now users can view more information about the movies

--- project2/App.js
+++ project2/App.js
@@ -97,9 +97,18 @@ class SearchResultsScreen extends React.Component {
 class Movie extends React.Component {
 
   render() {
+    const { genre_ids, id, original_title, overview, popularity, title, vote_average, vote_count } = this.props.route.params.movie
+    console.log(this.props.route.params.movie)
     return (
       <View>
-        <Text>Information about </Text>
+        <Text style={styles.searchText}>Information about {title}</Text>
+        <Text style={styles.searchText}>original title: {original_title}</Text>
+        <Text style={styles.searchText}>overview: {overview}</Text>
+        <Text style={styles.searchText}>popularity: {popularity}</Text>
+        <Text style={styles.searchText}>genre id's: {genre_ids}</Text>
+        <Text style={styles.searchText}>vote average: {vote_average}</Text>
+        <Text style={styles.searchText}>vote count: {vote_count}</Text>
+        <Text style={styles.searchText}>movie id: {id}</Text>
       </View>
     )
   }
